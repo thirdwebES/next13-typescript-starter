2475bdba1d0dc52aeabaa5644ad142179204b37c
Samu Sarmiento <samusarmiento@protonmail.com>
2023-07-24 18:15:48 +0200

    First Commit nextjs13-typescript-app template

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,108 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import Image from "next/image";
+import styles from "./page.module.css";
+import { ConnectWallet } from "@thirdweb-dev/react/evm";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
+      <div className={styles.container}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>
+            Welcome to{" "}
+            <span className={styles.gradientText0}>
+              <a
+                href="https://thirdweb.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                thirdweb.
+              </a>
+            </span>
+          </h1>
+
+          <p className={styles.description}>
+            Get started by configuring your desired network in{" "}
+            <code className={styles.code}>src/index.js</code>, then modify the{" "}
+            <code className={styles.code}>src/App.js</code> file!
+          </p>
+
+          <div className={styles.connect}>
+            <ConnectWallet
+              dropdownPosition={{
+                side: "bottom",
+                align: "center",
+              }}
+            />
+          </div>
+        </div>
+
+        <div className={styles.grid}>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://portal.thirdweb.com/"
+            className={styles.card}
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+              src="/images/portal-preview.png"
+              alt="Placeholder preview of starter"
+              width={300}
+              height={200}
             />
+            <div className={styles.cardText}>
+              <h2 className={styles.gradientText1}>Portal ➜</h2>
+              <p className={styles.paragraph}>
+                Guides, references, and resources that will help you build with
+                thirdweb.
+              </p>
+            </div>
+          </a>
+
+          <a
+            href="https://thirdweb.com/dashboard"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/images/dashboard-preview.png"
+              alt="Placeholder preview of starter"
+              width={300}
+              height={200}
+            />
+            <div className={styles.cardText}>
+              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
+              <p className={styles.paragraph}>
+                Deploy, configure, and manage your smart contracts from the
+                dashboard.
+              </p>
+            </div>
+          </a>
+
+          <a
+            href="https://thirdweb.com/templates"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/images/templates-preview.png"
+              alt="Placeholder preview of templates"
+              width={300}
+              height={200}
+            />
+            <div className={styles.cardText}>
+              <h2 className={styles.gradientText3}>Templates ➜</h2>
+              <p className={styles.paragraph}>
+                Discover and clone template projects showcasing thirdweb
+                features.
+              </p>
+            </div>
           </a>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
